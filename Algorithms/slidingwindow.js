c497bf71f26c5c61a8da4de8dc4c788afa90f70c
Carlos Velez <59077110+losvelle@users.noreply.github.com>
2021-03-10 00:05:17 -0500

    Sliding Window
    Using Sliding window to find the maximum sum of 4 numbers in a array of numbers.

---
+++ Algorithms/slidingwindow.js
@@ -0,0 +1,17 @@
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum
+}
+
+console.log(maxSubarraySum([2, 3, 5, 2, 4, 6, 7, 4, 2, 4, 6, 2, 3, 1, 2, 4, 5], 4))
